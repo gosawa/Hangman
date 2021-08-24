654a9837878991a758e12207ba49cdedaf008925
Goro Osawa <gosawa@gmail.com>
2021-08-24 18:35:39 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from random import randint
+
+class Game:
+    def __init__(self, word):
+        self.score = 5
+        self.spaces = len(word)
+        self.answer = word
+
+    def validate_single(self, x):
+        if len(x) != 1:
+            return False
+        if not x.isalpha():
+            return False
+
+    def inner_game(self):
+        while self.score > 0:
+            candidate = input("enter a letter")
+            if validate_single(candidate):
+                if candidate in self.answer:
+                    print("yay")
+                    print(f"your score is {self.score}")
+                else:
+                    print("Nooooo")
+                    self.score -= 1
+                    print(f"your score is {self.score}")
+
+
+bigstring = """This is more of a guess the word game the core concepts you have to use while
+developing this project are variables random integer strings char input and output and boolean 
+in the game users have to enter letter guesses and each user will have a limited number of guesses 
+a counter variable is needed for limiting the guesses this is one of the interesting python projects to begin with"""
+wordlist = bigstring.split()
+
+wordnum = randint(0, len(wordlist))
+
+this_game = Game(wordlist[wordnum])
+
+this_game.inner_game()
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
